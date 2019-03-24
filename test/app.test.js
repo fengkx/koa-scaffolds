5e8e0d613c0569a905028480901a293ca8efe8de
fengkx <liangkx8237@gmail.com>
2019-03-24 23:52:56 +0800

    pass test

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,7 @@
-const supertest = require('supertest');
-const {server}= require('../app');
 const test = require('ava');
+const supertest = require('supertest');
+const { server } = require('../app');
+
 const request = supertest(server);
 
 test('index', async (t) => {
@@ -9,15 +10,13 @@ test('index', async (t) => {
     t.is(res.status, 200);
     t.is(res.headers['content-type'], 'text/html; charset=utf-8');
     t.is(res.text, 'Hello index');
-})
+});
 
 test('post json', async (t) => {
     t.plan(3);
-    const obj = {message: 'Hello json', status: 'ok'};
-    const res = await request
-        .post('/json')
-        .send(obj);
+    const obj = { message: 'Hello json', status: 'ok' };
+    const res = await request.post('/json').send(obj);
     t.is(res.status, 200);
     t.is(res.headers['content-type'], 'application/json; charset=utf-8');
     t.deepEqual(res.body, obj);
-})
+});
